423cf1fd92f7c54eb972a390e8029e0097538d31
Roy <mutuaroymulinge@gmail.com>
2026-04-29 21:10:45 +0300

    feat: Create password variable -Add while loop. -Prompt user for password.

--- main.py
+++ main.py
@@ -1,31 +1,8 @@
-# user difined shopping list organizer
-# Ask user to enter items names and prices one by one 
+# Prompt user to enter password
+#  and validate the field and the user input
 
-is_running = True
-products = {}
+password = 'Roy'
 
-while is_running:
-    item = input('Enter the item bought or Type("q" to quit): ').lower()
-    if item == 'q':
-        is_running = False
-    else:
-        valid_price = True
-        if valid_price == True:
-            price = input('Enter the price of the item: ')
-            if not price.isdigit():
-             print('Price should be a digit')
-
-            products[item]= price
-            
-print(products)
-
-
+while True:
+    passwordInput = input("Enter your password: ")
     
-    
-
-
-
-
-
-
-
